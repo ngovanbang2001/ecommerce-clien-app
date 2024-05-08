56839590071d0de212155095a81c9698370b0a53
Ngo Van Bang <ngovanbang2001@gmail.com>
2024-05-08 22:36:06 +0700

    feat/add login page ,..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
-export default function RootLayout({
+import Footer from "@/components/basic/footer"
+import Header from "@/components/basic/header"
+import type { Metadata } from 'next'
 
-    children,
-}: {
-    children: React.ReactNode
-}) {
-    return (
-        <html lang="en">
-            <body>{children}</body>
-        </html>
-    )
+// These styles apply to every route in the application
+import '../styles/globals.css'
+import { Suspense } from "react"
+import Loading from "./loading"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const metadata: Metadata = {
+  title: 'E-commerce',
+  description: '',
+}
+
+type MainLayoutProps = {
+  children: React.ReactNode
+}
+
+export default function RootLayout({ children }: MainLayoutProps) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        <Suspense fallback={<Loading />}>
+          {children}
+          <ToastContainer />
+        </Suspense>
+        <Footer />
+      </body>
+    </html>
+  )
 }
